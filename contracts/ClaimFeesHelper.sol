c9c50d58fa9094659df4766772144ac02e1dc6a1
0xMaharishi <cm972646@gmail.com>
2022-06-02 20:38:50 +0100

    chore: import code4rena

--- contracts/ClaimFeesHelper.sol
+++ contracts/ClaimFeesHelper.sol
@@ -1,15 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity 0.8.11;
 
 import { IERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
-
-interface IFeeDistributor {
-    function claimToken(address user, IERC20 token) external returns (uint256);
-
-    function claimTokens(address user, IERC20[] calldata tokens) external returns (uint256[] memory);
-
-    function getTokenTimeCursor(IERC20 token) external view returns (uint256);
-}
+import { IFeeDistributor } from "./mocks/balancer/MockFeeDistro.sol";
 
 interface IBooster {
     function earmarkFees(address _feeDistro) external returns (bool);
